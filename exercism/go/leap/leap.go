1614739653ea8d2c2378eced6d2f530b1b28db6d
James Routley <jroutley@gmail.com>
2017-01-16 18:16:56 +0000

    Removing unnecessary else
    In the Go libraries, you'll find that when an if statement doesn't flow
    into the next statement—that is, the body ends in break, continue, goto,
    or return—the unnecessary else is omitted.

--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -12,11 +12,12 @@ const testVersion = 2
 func IsLeapYear(year int) bool {
 	if year%400 == 0 {
 		return true
-	} else if year%100 == 0 {
-		return false
-	} else if year%4 == 0 {
-		return true
-	} else {
+	}
+	if year%100 == 0 {
 		return false
 	}
+	if year%4 == 0 {
+		return true
+	}
+	return false
 }
