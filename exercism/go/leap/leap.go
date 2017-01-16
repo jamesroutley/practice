3cea2fbf48ee924a6cd704fbac8cb5e173a8ae16
James Routley <jroutley@gmail.com>
2017-01-16 18:09:31 +0000

    Linting

--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -3,31 +3,30 @@
 package leap
 
 import (
-  "math"
+	"math"
 )
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-
 func isDivisibleBy(x, y int) bool {
-  if math.Mod(float64(x), float64(y)) == 0 {
-    return true
-  } else {
-    return false
-  }
+	if math.Mod(float64(x), float64(y)) == 0 {
+	  return true
+	} else {
+		return false
+	}
 }
 
 // IsLeapYear which returns a boolean indicating whether a given year is a
 // leap year or not.
 func IsLeapYear(year int) bool {
 	if isDivisibleBy(year, 400) {
-    return true
-  } else if isDivisibleBy(year, 100) {
-    return false
-  } else if isDivisibleBy(year, 4) {
-    return true
-  } else {
-    return false
-  }
+		return true
+	} else if isDivisibleBy(year, 100) {
+		return false
+	} else if isDivisibleBy(year, 4) {
+		return true
+	} else {
+		return false
+	}
 }
