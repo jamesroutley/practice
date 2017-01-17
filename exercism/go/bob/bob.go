25cf008d85b576c55196c239d0255b5c574ee9ca
James Routley <jroutley@gmail.com>
2017-01-17 21:56:41 +0000

    go bob initial commit

---
+++ exercism/go/bob/bob.go
@@ -0,0 +1,9 @@
+// Package bob implements Bob, a lackadaisical teenager.
+package bob
+
+const testVersion = 2 // same as targetTestVersion
+
+// Hey returns responses to inputs
+func Hey(input string) string {
+  
+}
