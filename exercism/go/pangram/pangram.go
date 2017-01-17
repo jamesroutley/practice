a06cfeec5741afb6c763ebe12aada9361d019d26
James Routley <jroutley@gmail.com>
2017-01-17 20:33:49 +0000

    Adding go pangram solution

--- exercism/go/pangram/pangram.go
+++ exercism/go/pangram/pangram.go
@@ -1,5 +1,35 @@
+// Package pangram implements a function which returns whether a sentence is a
+// pangram
 package pangram
+
+import (
+	"regexp"
+	"strings"
+)
 
 const testVersion = 1
 
-func IsPangram(string) bool
+// ACSII codes for the letters "a" and "z"
+const (
+	aASCII = 97
+	zASCII = 122
+)
+
+// Pattern matching lower and uppercase English letters
+var asciiLettersRe = regexp.MustCompile("[^a-zA-Z]")
+
+// IsPangram returns a boolean, indicating whether sentence is a pangram.
+func IsPangram(sentence string) bool {
+	s := []byte(strings.ToLower(sentence))
+	s = asciiLettersRe.ReplaceAll(s, []byte(""))
+	alphabet := make(map[byte]bool)
+	for _, letter := range s {
+		alphabet[letter] = true
+	}
+	for i := aASCII; i <= zASCII; i++ {
+		if alphabet[byte(i)] == false {
+			return false
+		}
+	}
+	return true
+}
