8dcffc949c6ffc7ff01dbf8a2704cdd7c592c0ad
James Routley <jroutley@gmail.com>
2017-01-17 13:00:23 +0000

    Adding raindrops solution

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,7 +1,29 @@
+// Package raindrops implements a number to raindrop-speak converter
 package raindrops
 
+import (
+	"bytes"
+	"strconv"
+)
+
+// Define which test version to use
 const testVersion = 2
 
-func Convert(int) string
+// Convert converts a number to a string, following the raindrop rules
+func Convert(n int) string {
+	var response bytes.Buffer
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+	if n%3 == 0 {
+		response.WriteString("Pling")
+	}
+	if n%5 == 0 {
+		response.WriteString("Plang")
+	}
+	if n%7 == 0 {
+		response.WriteString("Plong")
+	}
+	if len(response.Bytes()) == 0 {
+		response.WriteString(strconv.Itoa(n))
+	}
+	return response.String()
+}
