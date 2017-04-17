dabb26860ec73fe95fffad07ccd2fd260120dc1f
James Routley <jroutley@gmail.com>
2017-04-17 12:18:12 +0200

    Add early lcthw exercises

---
+++ lcthw/ex3.c
@@ -0,0 +1,11 @@
+#import <stdio.h>
+
+int main() {
+    zint age = 10;
+    int height = 72;
+
+    printf("I am %d years old\n", age);
+    printf("I am %d inches tall\n", height);
+
+    return 0;
+}
